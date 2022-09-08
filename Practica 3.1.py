a2ee0e1f0f328ea52a638009ceadfcdb09ec3dd5
Ian <ianvelazquez117@gmail.com>
2022-09-08 17:51:02 -0500

    Try generate CSV

--- Practica 3.1.py
+++ Practica 3.1.py
@@ -1,6 +1,7 @@
 # Importes 
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+import pandas as pd
 
 # Seleccionar el WebDriver y definir la URL
 d = webdriver.Chrome(executable_path=r"C:\Users\Dell\Documents\IAN\Universidad\7 Semestre\Calidad de Software\chromedriver.exe")
@@ -13,5 +14,30 @@ mx.click()
 r = d.find_element(By.CLASS_NAME, "title")
 r.click()
 
-sel = d.find_element(By.XPATH, "/html[1]/body[1]/div[5]/div[1]/div[4]/div[1]/section[4]/section[1]/div[1]/article[1]/section[1]/ul[1]/li[2]/h2[1]/a[1]")
+sel = d.find_element(By.XPATH, '//*[@id="cityTable"]/div/article/section/ul[1]/li[2]')
 sel.click()
+
+# time.sleep(3)
+
+txtcolumns  = d.find_element(By.CLASS_NAME, 'm_table_weather_hour_day').text
+# txtcolumns  = d.find_element(By.XPATH, '//*[@id="cityTable"]/div/article/section/ul[1]/li[2]').text
+# print("\n --> ", txtcolumns.split('Hoy'))
+
+lista = list(txtcolumns.split('\n'))
+todays_weather = txtcolumns.split('Hoy')[0].split('\n')[1:-1]
+print(todays_weather)
+print(lista)
+
+horas=list()
+temp=list()
+v_viento=list()
+
+for i in range(0,len(todays_weather),3):
+    horas.append(todays_weather[i])
+    temp.append(todays_weather[i+1])
+    v_viento.append(todays_weather[i+2])
+
+df = pd.DataFrame({'Horas': horas, 'Temperatura': temp, 'Velocidad_viento (Km/h)': v_viento})
+
+print(df)
+df.to_csv('tiempo_hoy.csv', index=False)
